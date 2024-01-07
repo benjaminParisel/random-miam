657c7b1f79c8d9ac1dd4b46422e82ba7ac496adc
benjaminparisel <benjamin.parisel@bonitasoft.com>
2024-01-07 14:48:13 +0100

    styles: improve layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,48 +16,48 @@ export default async function Header() {
   const session = await getServerAuthSession(); //(1)
 
   return (
-    <div className="flex items-center bg-gray-800">
-      <div className="flex flex-1 sm:items-stretch sm:justify-start">
-        <div className="flex flex-shrink-0 items-center">
-          <Link href="/">
-            <Image
-              src="/random-miam.png"
-              alt="Random-miam"
-              width={50}
-              height={50}
-            />
-          </Link>
-        </div>
-        <div className="hidden sm:ml-6 sm:block">
-          <div className="flex space-x-4">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className={cn(
-                  item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'rounded-md px-3 py-2 text-sm font-medium'
-                )}
-                aria-current={item.current ? 'page' : undefined}
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
+    <div className="flex items-center bg-gray-800 min-h-16">
+      <div className="min-w-4 p-2">
+        <Link href="/">
+          <Image
+            src="/random-miam.png"
+            alt="Random-miam"
+            width={50}
+            height={50}
+          />
+        </Link>
+      </div>
+      <div className="hidden sm:ml-6 sm:block flex-grow">
+        <div className="flex space-x-4">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className={cn(
+                item.current
+                  ? 'bg-gray-900 text-white'
+                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'rounded-md px-3 py-2 text-sm font-medium'
+              )}
+              aria-current={item.current ? 'page' : undefined}
+            >
+              {item.name}
+            </a>
+          ))}
         </div>
       </div>
-      <div className="flex flex-row gap-3 items-center">
-        {!session && (
-          <Link
-            className="font-medium mt-2 text-blue-600 hover:underline"
-            href="/login"
-          >
-            Login
-          </Link>
-        )}
-        {session && <UserHeader user={session?.user} />}
+      <div className="flex justify-end min-w-28 px-2">
+        <div className="flex flex-row gap-3 items-center">
+          {!session && (
+            <Link
+              className="font-medium mt-2 text-blue-600 hover:underline"
+              href="/login"
+            >
+              Login
+            </Link>
+          )}
+          {session && <UserHeader user={session?.user} />}
+        </div>
       </div>
     </div>
   );
